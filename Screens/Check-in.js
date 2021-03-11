e704e6741e1937e7a0b564cbebb2aa5e2bab1ec6
sohaibusmani <50169776+sohaibusmani@users.noreply.github.com>
2021-03-11 13:27:25 +0500

    dropdown items css styled

--- Screens/Check-in.js
+++ Screens/Check-in.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
+import { Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 function CheckIn({navigation}){
@@ -10,12 +14,13 @@ function CheckIn({navigation}){
     <View style={{flex:1}}>
         <DropDownPicker
     items={[
-        {label: 'USA', value: 'usa'},
-        {label: 'UK', value: 'uk', },
-        {label: 'France', value: 'france'},
+        {label: 'Check-In', value: 'checkIn', icon: () => <Feather name="check" size={18} color='black' />},
+        {label: 'Check-Out', value: 'checkOut', icon: () => <MaterialIcons name="exit-to-app" size={18} color="black" /> },
+        {label: 'Break-Start', value: 'breakStart', icon: () => <MaterialIcons name="free-breakfast" size={18} color='black' />},
+        {label: 'Break-End', value: 'breakEnd', icon: () => <MaterialCommunityIcons name="format-page-break" size={18} color='black' />},
     ]}
     defaultValue={criteria}
-    containerStyle={{height: 40}}
+    containerStyle={{height: 70}}
     style={{backgroundColor: '#fafafa'}}
     itemStyle={{
         justifyContent: 'flex-start'

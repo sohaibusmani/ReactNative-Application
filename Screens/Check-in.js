9e212dbd65f85e91d7d47879f9e093e89c1ac83d
sohaibusmani <50169776+sohaibusmani@users.noreply.github.com>
2021-03-11 19:02:14 +0500

    create card on check-in screen

--- Screens/Check-in.js
+++ Screens/Check-in.js
@@ -1,37 +1,91 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
+import { Button, Card } from 'react-native-paper';
 import { Feather } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useEffect } from 'react';
+import { Text } from 'native-base';
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+         justifyContent: 'center'
+    },
+    btn: {
+        margin: 20,
+        width: '60%',
+        backgroundColor: '#66bbb9'
+    },
+    card: {
+        margin: 10,
+        backgroundColor: '#66bbb9',
+        borderRadius: 10,
+        height: 100
+    }
+
+})
 
 
-function CheckIn({navigation}){
-    const [criteria, setCriteria] = React.useState('')
-    console.log(criteria);
+function CheckIn({ navigation }) {
+    const [criteria, setCriteria] = React.useState('');
+    const [currentDate, setCurrentDate] = React.useState('');
 
-    return(
-    <View style={{flex: 1, justifyContent: 'center'}}>
-        <DropDownPicker
-    items={[
-        {label: 'Check-In', value: 'checkIn', icon: () => <Feather name="check" size={18} style={{marginEnd:10}} color='black' />},
-        {label: 'Check-Out', value: 'checkOut', icon: () => <MaterialIcons name="exit-to-app" style={{marginEnd:10}} size={18} color="black" /> },
-        {label: 'Break-Start', value: 'breakStart', icon: () => <MaterialIcons name="free-breakfast" style={{marginEnd:10}} size={18} color='black' />},
-        {label: 'Break-End', value: 'breakEnd', icon: () => <MaterialCommunityIcons name="format-page-break" style={{marginEnd:10}} size={18} color='black' />},
-    ]}
-    defaultValue={criteria}
-    containerStyle={{height: 70}}
-    style={{backgroundColor: '#fafafa'}}
-    itemStyle={{
-        justifyContent: 'space-between',
-        padding: 10
+    
 
-    }}
-    dropDownStyle={{backgroundColor: '#fafafa'}}
-    onChangeItem={item => setCriteria(item.value)}
-/>
-    </View>
+      const getDate = () => {
+        var date = new Date().getDate(); //Current Date
+        var month = new Date().getMonth() + 1; //Current Month
+        var year = new Date().getFullYear(); //Current Year
+        var hours = new Date().getHours(); //Current Hours
+        var min = new Date().getMinutes(); //Current Minutes
+        var sec = new Date().getSeconds(); //Current Seconds
+        setCurrentDate(
+          date + '/' + month + '/' + year 
+          + ' ' + hours + ':' + min + ':' + sec
+        );
+      }
+    
+
+    return (
+        <View style={styles.screen}>
+            <DropDownPicker
+                items={[
+                    { label: 'Check-In',  value: 'checkIn', icon: () => <Feather name="check" size={18} style={{ marginEnd: 10 }} color='black' /> },
+                    { label: 'Check-Out', value: 'checkOut', icon: () => <MaterialIcons name="exit-to-app" style={{ marginEnd: 10 }} size={18} color="black" /> },
+                    { label: 'Break-Start', value: 'breakStart', icon: () => <MaterialIcons name="free-breakfast" style={{ marginEnd: 10 }} size={18} color='black' /> },
+                    { label: 'Break-End', value: 'breakEnd', icon: () => <MaterialCommunityIcons name="format-page-break" style={{ marginEnd: 10 }} size={18} color='black' /> },
+                ]}
+                defaultValue={criteria}
+                containerStyle={{ height: 70 }}
+                style={{ backgroundColor: '#fafafa' }}
+                itemStyle={{
+                    justifyContent: 'space-between',
+                    padding: 10, 
+
+                }}
+                dropDownStyle={{ backgroundColor: '#fafafa' }}
+                onChangeItem={item => setCriteria(item.value)}
+            />
+            <Button mode='contained' style={styles.btn} onPress={getDate}>
+                Start
+            </Button>
+            <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+                 <Card style={styles.card}>
+                     <Card.Content>
+                         <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', padding: 20}}>
+                             <Text>
+                                 {criteria}
+                             </Text>
+                             <Text>
+                                 {currentDate}
+                             </Text>
+                         </View>
+                     </Card.Content>
+                 </Card>
+               </TouchableOpacity>
+        </View>
     )
 }
 

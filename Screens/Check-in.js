be03fb6c4f394e0c7c41a6b5015d71a79c8df91d
sohaibusmani <50169776+sohaibusmani@users.noreply.github.com>
2021-03-27 16:54:11 +0500

    async storage installed

--- Screens/Check-in.js
+++ Screens/Check-in.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, ImageBackground, Text, TouchableOpacity, ScrollView} from 'react-native';
+import { View, StyleSheet, ImageBackground, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import { Feather } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -10,116 +10,117 @@ import image from '../assets/pic2.jpg';
 
 
 const styles = StyleSheet.create({
-    screen:{
-        flex: 1,
-         justifyContent: 'center'
-    },
-    btn: {
-        margin: 20,
-        backgroundColor: '#66bbb9',
-        height: 38
-    },
-    card: {
-        margin: 10,
-        backgroundColor: '#66bbb9',
-        borderRadius: 10,
-        height: 100
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-        flex: 1
-      },
-    appButtonContainer: {
-        elevation: 8,
-        backgroundColor: "#009688",
-        // borderRadius: 10,
-        paddingVertical: 10,
-        paddingHorizontal: 12,
-        width: '60%',
-    },
-    appButtonText: {
-        fontSize: 18,
-        color: "#fff",
-        fontWeight: "bold",
-        alignSelf: "center",
-        textTransform: "uppercase"
-    }
+  screen: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  btn: {
+    margin: 20,
+    backgroundColor: '#66bbb9',
+    height: 38
+  },
+  card: {
+    margin: 10,
+    backgroundColor: '#66bbb9',
+    borderRadius: 10,
+    height: 100
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    // borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: '60%',
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  }
 })
 
- 
+
 function CheckIn({ navigation }) {
-    const [criteria, setCriteria] = React.useState([])
-    const [currentDate, setCurrentDate] = React.useState('');
-    const [showCard, setShowCard] = React.useState(false);
-    
+  const [checkIn, setCheckIn] = React.useState('');
+  const [breakStart, setBreakStart] = React.useState('');
+  const [breakEnd, setBreakEnd] = React.useState('');
+  const [checkOut, setCheckOut] = React.useState('');
+  const [currentDate, setCurrentDate] = React.useState('');
+  const [showCard, setShowCard] = React.useState(false);
 
-      const getDate = () => {
-        var date = new Date().getDate(); //Current Date
-        var month = new Date().getMonth() + 1; //Current Month
-        var year = new Date().getFullYear(); //Current Year
-        var hours = new Date().getHours(); //Current Hours
-        var min = new Date().getMinutes(); //Current Minutes
-        var sec = new Date().getSeconds(); //Current Seconds
-        setCurrentDate(
-          date + '/' + month + '/' + year 
-          + ' ' + hours + ':' + min + ':' + sec
-        );
-        setShowCard(true);
-      }
 
-      const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+  const getDate = () => {
+    var date = new Date().getDate(); //Current Date
+    var month = new Date().getMonth() + 1; //Current Month
+    var year = new Date().getFullYear(); //Current Year
+    var hours = new Date().getHours(); //Current Hours
+    var min = new Date().getMinutes(); //Current Minutes
+    var sec = new Date().getSeconds(); //Current Seconds
+    setCurrentDate(
+      date + '/' + month + '/' + year
+      + ' ' + hours + ':' + min + ':' + sec
+    );
+    setShowCard(true);
+  }
 
-    return (
-        <View style={{flex: 1}}>
-          <ImageBackground resizeMode='cover' style={styles.image} style={{height: 250}} source={image}>
-              <View style={{flex:1, backgroundColor:'rgba(0,0,0,0.3)',}}>
-                <View style={{color:'white', alignItems:'center'}}>
-                <Title style={{ color:'white',}}>20 March 2021</Title>
+  return (
+    <View style={{ flex: 1 }}>
+      <ImageBackground resizeMode='cover' style={styles.image} style={{ height: 250 }} source={image}>
+        <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.3)', }}>
+          <View style={{ color: 'white', alignItems: 'center' }}>
+            <Title style={{ color: 'white', }}>20 March 2021</Title>
+          </View>
+          <View style={{ flexDirection: 'row', margin: 15 }}>
+            <Feather style={{ marginTop: 2 }} name="check-square" size={20} color="white" />
+            <Text style={{ marginLeft: 8, fontSize: 18, color: 'white' }}>Check In : 9:05 AM</Text>
+          </View>
+          <View style={{ flexDirection: 'row', margin: 15 }}>
+            <MaterialIcons name="free-breakfast" size={24} color="white" />
+            <Text style={{ marginLeft: 8, fontSize: 18, color: 'white' }}>Break Start : 2:05 PM</Text>
+          </View>
+          <View style={{ flexDirection: 'row', margin: 15 }}>
+            <MaterialCommunityIcons name="format-page-break" size={24} color="white" />
+            <Text style={{ marginLeft: 8, fontSize: 18, color: 'white' }}>Break End : 3:05 PM</Text>
+          </View>
+          <View style={{ flexDirection: 'row', margin: 15 }}>
+            <MaterialIcons name="exit-to-app" size={23} color="white" />
+            <Text style={{ marginLeft: 8, fontSize: 18, color: 'white' }}>Check Out : 8:05 PM</Text>
+          </View>
+        </View>
+      </ImageBackground>
+      <ScrollView>
+        <View style={{ marginTop: 20 }}>
+          <View>
+            <Card>
+              <Card.Title title="Sohaib Usmani" subtitle="Employee" />
+              <Card.Content>
+                <View style={{ flexDirection: 'row' }}>
+                  <Entypo name="back-in-time" size={20} color="black" />
+                  <Text style={{ marginLeft: 5 }}>9:05 AM</Text>
                 </View>
-                <View style={{flexDirection:'row', margin: 15}}>
-                <Feather style={{marginTop: 2}} name="check-square" size={20} color="white" />
-                <Text style={{marginLeft: 8, fontSize: 18, color:'white'}}>Check In : 9:05 AM</Text>
-                </View>
-                <View style={{flexDirection:'row', margin: 15}}>
-                <MaterialIcons name="free-breakfast" size={24} color="white" />
-                <Text style={{marginLeft: 8, fontSize: 18, color:'white'}}>Break Start : 2:05 PM</Text>
-                </View>
-                <View style={{flexDirection:'row', margin: 15}}>
-                <MaterialCommunityIcons name="format-page-break" size={24} color="white" />
-                <Text style={{marginLeft: 8, fontSize: 18, color:'white'}}>Break End : 3:05 PM</Text>
-                </View>
-                <View style={{flexDirection:'row', margin: 15}}>
-                <MaterialIcons name="exit-to-app" size={23} color="white" />
-                <Text style={{marginLeft: 8, fontSize: 18, color:'white'}}>Check Out : 8:05 PM</Text>
-                </View>    
-              </View>
-          </ImageBackground>
-          <ScrollView>
-          <View style={{marginTop: 20}}>
-            <View>
-          <Card>
-    <Card.Title title="Sohaib Usmani" subtitle="Employee" />
-    <Card.Content>
-      <View style={{flexDirection: 'row'}}>
-    <Entypo name="back-in-time" size={20} color="black" /> 
-    <Text style={{marginLeft: 5}}>9:05 AM</Text>
-    </View>
-      <Paragraph>Checked In</Paragraph>
-    </Card.Content>
-  </Card>
-  </View>
-  <View style={{marginTop: 20, alignItems:'center'}}>
+                <Paragraph>Checked In</Paragraph>
+              </Card.Content>
+            </Card>
+          </View>
+          <View style={{ marginTop: 20, alignItems: 'center' }}>
             <TouchableOpacity style={styles.appButtonContainer}>
               <Text style={styles.appButtonText}>
                 Break
               </Text>
             </TouchableOpacity>
-            </View>
           </View>
-        </ScrollView>
         </View>
-    )
+      </ScrollView>
+    </View>
+  )
 }
 
 export default CheckIn;

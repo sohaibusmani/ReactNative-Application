6f5ee4e785206fb47928444621543fe9ec1b4e38
sohaibusmani <50169776+sohaibusmani@users.noreply.github.com>
2021-04-02 13:33:28 +0500

    Code updated expo splash screen removed

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import {NativeRouter, Switch, Route} from 'react-router-native';
-import {View, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
@@ -10,37 +8,19 @@ import CheckInStack from './Routes/CheckInStack';
 const Drawer = createDrawerNavigator();
 
 
-const styles = StyleSheet.create({
-  screen: {
-      alignItems: 'center',
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor:'rgba(0,0,0,0.3)', 
-  },
-  inputContainer: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '80%',
-
-  },
-});
-
-
 // Components 
-import Splash from './Screens/Splash';
 import MyDrawer from './Components/Drawer';
 
 
 export default function App() {
 
   return (
-    // <NavigationContainer>
-    // <Drawer.Navigator drawerContent={props => <MyDrawer {...props} />}>
-    //   <Drawer.Screen name="Home" component={HomeStack} />
-    //   <Drawer.Screen name="CheckIn" component={CheckInStack} />
-    // </Drawer.Navigator>
-    // </NavigationContainer>
-     <Splash/>
+    <NavigationContainer>
+    <Drawer.Navigator drawerContent={props => <MyDrawer {...props} />}>
+      <Drawer.Screen name="Home" component={HomeStack} />
+      <Drawer.Screen name="CheckIn" component={CheckInStack} />
+    </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
